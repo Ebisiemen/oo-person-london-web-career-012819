f97165a78db47e188ac9d8c65e6a4ce1379948de
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-01-31 13:04:17 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,29 @@
-# your code goes here
+class Person
+
+  attr_reader :name
+  attr_accessor :bank_account, :happiness 
+
+  def initialize(name)
+    @name = name
+    @happiness = 8
+    @hygiene = 8
+    @bank_account = 25
+  end   
+end   
+
+
+  
+#   def hygiene =(num)
+#   @hygiene = num
+#   @hygiene = 10 if @hygiene > 10
+#   @hygiene = 0 if @hygiene < 0
+# end
+
+# def happy ?
+#   happiness > 7
+# end
+
+# def clean ?
+#   hygiene > 7
+# end
+end
